c7d365a15221686286a8c93d1b4ac99527eb0f9b
Lorenss Teo Lundgrenn <61320783+LorenssLundgrenn@users.noreply.github.com>
2022-11-06 15:52:06 +0100

    all routes and controllers implemented

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,35 +3,108 @@ const UserModel = db.userModel;
 
 exports.create = (req, res) => {
     //create new user and save to db
-    const user = UserModel({ 
-        name: req.body.name, 
+    let newUser = UserModel({
+        name: req.body.name,
         age: req.body.age,
         alive: req.body.alive ? req.body.alive : true
     });
-    user.save(user)
+    newUser.save(newUser)
     .then(data => {
         res.send(data);
-        //find added user by id
-        UserModel.findById(user._id, (err, res) => {
-            if (err) { console.log(`error: ${err}`) }
-            else { console.log(`new user:\n${res}`) }
-        });
     })
     .catch(err => {
         res.status(500).send({ message:`error: ${err.message}` })
-        process.exit();
     });
 };
 
 exports.findAll = (req, res) => {
     let reqName = req.body.name;
-    UserModel.find({ name: reqName })
+    UserModel.find(reqName ? { name: reqName } : {} )
         .then(data => {
         res.send(data);
-        console.log(`all users named ${reqName}:\n${data}`);
         })
         .catch(err => {
             res.status(500).send({ message:`error: ${err.message}` })
-            process.exit();
+        });
+}
+
+exports.findOne = (req, res) => {
+    let reqId = req.params.id;
+    UserModel.findById(reqId)
+        .then(data => {
+            if (data){
+                res.send(data);
+            } else {
+                res.status(404).send({ message: `could not find ${reqId}`});
+            }
+        }).catch(err => {
+            res.status(500).send({ message:`error: ${err.message}` })
+        });
+}
+
+exports.update = (req, res) => {
+    let reqId = req.params.id;
+    UserModel.findByIdAndUpdate(reqId, req.body, {useFindAndModify: false})
+    .then(data => {
+        if (data) {
+            res.send({ message: `user ${reqId} updated successfully`});
+        } else {
+            res.status(404).send({
+                message: `Cannot update user ${reqId}. Perhaps not found`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: `Error updating user ${reqId}`
+        });
     });
-};
+}
+
+exports.delete = (req, res) => {
+    let reqId = req.params.id;
+    UserModel.findByIdAndDelete(reqId)
+    .then(data => {
+        if (data) {
+            res.send({
+                message: `successfully deleted user ${reqId}`
+            });
+        } else {
+            res.status(404).send({
+                message: `Cannot delete user ${reqId}. Perhaps not found`
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: `failed to delete user ${reqId}`
+        });
+    });
+}
+
+exports.deleteAll = (req, res) => {
+    UserModel.deleteMany({})
+      .then(data => {
+        res.send({
+          message: `${data.deletedCount} users were successfully deleted.`
+        });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: err.message || "some error occurred while removing all users"
+        });
+      });
+  };
+
+exports.findAllAlive = (req, res) => {
+    UserModel.find({ alive: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "some error occurred while retrieving all living users."
+      });
+    });
+}
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,9 +2,13 @@ module.exports = app => {
     const userController = require("../controllers/user.controller");
     var router = require("express").Router();
 
-    //create post and get routes
     router.post("/", userController.create);
     router.get("/", userController.findAll);
+    router.get("/alive", userController.findAllAlive);// <- order matters
+    router.get("/:id", userController.findOne);// <-------- ^^^^^^^^^^^^^
+    router.put("/:id", userController.update);
+    router.delete("/:id", userController.delete);
+    router.delete("/", userController.deleteAll);
 
-    app.use("/users", router); //link router to main express object
+    app.use("/api/users", router); //link router to main express object
 }
